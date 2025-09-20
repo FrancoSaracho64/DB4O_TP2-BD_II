436cbe94a11bd8d0055a63b2a72a95562297c5f5
FrancoSaracho64 <fmsaracho64@gmail.com>
2025-09-20 01:43:34 -0300

    Ajustes varios

--- src/main/java/ar/edu/unlu/bdd/Main.java
+++ src/main/java/ar/edu/unlu/bdd/Main.java
@@ -2,19 +2,15 @@ package ar.edu.unlu.bdd;
 
 import ar.edu.unlu.bdd.menu.menu;
 import ar.edu.unlu.bdd.utils.Utildb;
-import com.db4o.ObjectContainer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        ObjectContainer db = Utildb.getDb();
         try {
             Utildb.initEmbebido();
             new menu();
         } finally {
-            db.close(); // cerrar conexión siempre
+            Utildb.close();
+            System.out.println("Programa finalizado.");
         }
     }
 }
--- src/main/java/ar/edu/unlu/bdd/utils/Utildb.java
+++ src/main/java/ar/edu/unlu/bdd/utils/Utildb.java
@@ -10,30 +10,36 @@ public class Utildb {
     private static ObjectServer server = null;
 
     public static void initServer() {
-        try{
-            server = Db4oClientServer.openServer("mibase.db",8080);
-            server.grantAccess("usuario","contrasenia");
+        try {
+            server = Db4oClientServer.openServer("mibase.db", 8080);
+            server.grantAccess("usuario", "contrasenia");
             System.out.println("Server db4o running...");
             System.out.println("at localhost,port 8080,database mibase.db ...");
             System.out.println("Press any key to stop");
             System.in.read();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
         } finally {
             server.close();
         }
     }
+
     public static void initEmbebido() {
-        db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),"mibase.db");
+        db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "mibase.db");
     }
+
     public static void initClient() {
-        try{
+        try {
             db = Db4oClientServer.openClient("localhost", 8080, "usuario", "contrasenia");
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
     }
-    public static ObjectContainer getDb() { return db; }
+
+    public static ObjectContainer getDb() {
+        return db;
+    }
+
     public static void close() {
         System.out.println("close!");
         db.close();
