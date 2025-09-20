5e586030e4ee3c852ff5a91860d808a390c1c809
gustavo <lito.contardi@hotmail.com>
2025-09-19 23:30:17 -0300

    esta locuraaaaaaaaaa

--- src/main/java/ar/edu/unlu/bdd/entidad/Cliente.java
+++ src/main/java/ar/edu/unlu/bdd/entidad/Cliente.java
@@ -1,4 +1,30 @@
 package ar.edu.unlu.bdd.entidad;
 
 public class Cliente {
+    private Integer id;
+    private String descr;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String descr) {
+        this.id = id;
+        this.descr = descr;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
 }
