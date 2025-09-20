c1287e0f9986c82ff369c66ad264d9d6c1f064b4
gustavo <lito.contardi@hotmail.com>
2025-09-19 23:29:43 -0300

    esta locuraaaaaaaaaa

---
+++ src/main/java/ar/edu/unlu/bdd/entidad/Factura.java
@@ -0,0 +1,40 @@
+package ar.edu.unlu.bdd.entidad;
+
+public class Factura {
+    private Integer nro;
+    private Integer id;
+    private Double importe;
+
+    public Factura(Integer nro, Integer id, Double importe) {
+        this.nro = nro;
+        this.id = id;
+        this.importe = importe;
+    }
+
+    public Factura() {
+    }
+
+    public Integer getNro() {
+        return nro;
+    }
+
+    public void setNro(Integer nro) {
+        this.nro = nro;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(Double importe) {
+        this.importe = importe;
+    }
+}
