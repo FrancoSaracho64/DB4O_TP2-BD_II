d36cfd301a859c3d3853ef8ae3e6000fd928f8de
FrancoSaracho64 <fmsaracho64@gmail.com>
2025-09-21 19:00:52 -0300

    Conexión a la base

--- ejerciciotres/src/main/java/ar/edu/unlu/bdd/MainEjercicioTres.java
+++ ejerciciotres/src/main/java/ar/edu/unlu/bdd/MainEjercicioTres.java
@@ -2,9 +2,21 @@ package ar.edu.unlu.bdd;
 
 import ar.edu.unlu.bdd.menu.Menu;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class MainEjercicioTres {
     public static void main(String[] args) {
         try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("firebirdPU");
+
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.createNativeQuery("SELECT 1 FROM RDB$DATABASE").getSingleResult(); // consulta mínima a Firebird
+            em.getTransaction().commit();
+
+
             new Menu();
         } finally {
             System.out.println("Programa finalizado.");
