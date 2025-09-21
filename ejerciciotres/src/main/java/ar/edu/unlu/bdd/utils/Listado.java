112805d15a6e72f3f8269f3ad624d7d2e8c89b2b
MattZander24 <mattzanderm24mail.com>
2025-09-21 19:34:18 -0300

    Listado

---
+++ ejerciciotres/src/main/java/ar/edu/unlu/bdd/utils/Listado.java
@@ -0,0 +1,19 @@
+package ar.edu.unlu.bdd.utils;
+
+public class Listado {
+    private Listado() {
+    }
+
+    public static void listarMenu(String nombreTabla) {
+        System.out.println("\n----------------------------------------------");
+        System.out.println(" - " + nombreTabla + " - ");
+        System.out.println("Seleccione la opción que desea realizar sobre la entidad");
+        System.out.println("1 - Alta");
+        System.out.println("2 - Baja");
+        System.out.println("3 - Modificación");
+        System.out.println("4 - Consultar");
+        System.out.println("5 - Volver");
+        System.out.print("");
+    }
+}
+
